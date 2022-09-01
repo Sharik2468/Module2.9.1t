7ae5053f9d1f1dbf58b886624ae75eed799facde
Sharik2468 <nsharabanov@bk.ru>
2022-09-01 23:43:24 +0300

    Initial commit

---
+++ Module9.1t/Module9.1t.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <future>
+#include <random>
+
+int GetRandomIntInRange(const int min, const int max)
+{
+	static std::default_random_engine gen(
+		static_cast<unsigned>(
+			std::chrono::system_clock::now().time_since_epoch().count()
+			)
+	);
+	std::uniform_int_distribution<int> distribution(min, max);
+	return distribution(gen);
+}
+
+int GenerateValue(int value)
+{
+	return GetRandomIntInRange(0, 80) + value;
+}
+
+int main()
+{
+	std::future<int> Producer = std::async(GenerateValue, 10);
+	std::cout << Producer.get();
+}
